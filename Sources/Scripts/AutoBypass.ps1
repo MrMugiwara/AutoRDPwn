08370f5e1fe222b7a8a8c5e7ff127449eb2ca47e
JoelGMSec <34335312+JoelGMSec@users.noreply.github.com>
2019-01-16 15:48:27 +0100

    Update AutoBypass.ps1

--- Sources/Scripts/AutoBypass.ps1
+++ Sources/Scripts/AutoBypass.ps1
@@ -10,6 +10,7 @@ REG ADD "HKCU\Console" /v FontWeight /t REG_DWORD /d 700 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ScreenBufferSize /t REG_DWORD /d 589889636 /f 2>&1> $null
 REG ADD "HKCU\Console" /v FontSize /t REG_DWORD /d 1048576 /f 2>&1> $null
 REG ADD "HKCU\Console" /v WindowSize /t REG_DWORD /d 1966180 /f 2>&1> $null
+REG ADD "HKCU\Console" /v WindowAlpha /t REG_DWORD /d 231 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable00 /t REG_DWORD /d 789516 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable01 /t REG_DWORD /d 14300928 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable02 /t REG_DWORD /d 958739 /f 2>&1> $null
