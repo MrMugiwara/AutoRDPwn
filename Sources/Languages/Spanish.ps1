b83ff10592478918c252d3218376445ae6945e05
JoelGMSec <34335312+JoelGMSec@users.noreply.github.com>
2019-03-26 16:05:13 +0100

    Update Spanish.ps1

--- Sources/Languages/Spanish.ps1
+++ Sources/Languages/Spanish.ps1
@@ -1,4 +1,4 @@
-﻿[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
+[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
  
 $txt1  = "Cargar módulos adicionales"
 $txt2  = "Cerrar el programa"
